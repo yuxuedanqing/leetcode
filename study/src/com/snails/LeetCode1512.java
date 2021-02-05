e2db080cc9a8b5d400b3a9978b129f5915f54a99
snails <202292735@qq.com>
2021-02-05 16:42:59 +0800

    Leet Code 1512 好数对的数目

---
+++ study/src/com/snails/LeetCode1512.java
@@ -0,0 +1,28 @@
+package com.snails;
+
+/**
+ * https://leetcode-cn.com/problems/number-of-good-pairs/
+ * 1512. 好数对的数目
+ */
+public class LeetCode1512 {
+
+    public int numIdenticalPairs(int[] nums) {
+        int flag = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int a = nums[i];
+            for (int j=i+1;j<nums.length;j++){
+                int b = nums[j];
+                if (a == b){
+                    flag++;
+                }
+            }
+        }
+        return flag;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,1};
+        int i = new LeetCode1512().numIdenticalPairs(nums);
+        System.out.println(i);
+    }
+}
