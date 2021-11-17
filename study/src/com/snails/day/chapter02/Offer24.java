854fc4315e0c00589e7fcfe92484f1eec64122ec
Mkydy <2062771709@qq.com>
2021-11-17 22:26:48 +0800

    feat(Day2): 剑指Offer 24 35 06题
    1.剑指 Offer 06. 从尾到头打印链表
    2.剑指 Offer 24. 反转链表
    3.剑指 Offer 35. 复杂链表的复制

---
+++ study/src/com/snails/day/chapter02/Offer24.java
@@ -0,0 +1,55 @@
+package com.snails.day.chapter02;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author Snails
+ * @date 2021/11/17 20:05
+ *
+ * 剑指 Offer 24. 反转链表
+ *
+ * 定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+ *
+ * 示例:
+ *
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ *
+ *
+ * 限制：
+ *
+ * 0 <= 节点个数 <= 5000
+ */
+public class Offer24 {
+
+    private LinkedList<ListNode> first = new LinkedList<>();
+
+    public ListNode reverseList(ListNode head) {
+        ListNode cur = head, pre = null;
+        while (null != cur) {
+            ListNode tmp = cur.next;    // 暂存后继节点 cur.next
+            cur.next = pre;             // 修改 next 引用指向
+            pre = cur;                  // pre 暂存 cur
+            cur = tmp;                  // cur 访问下一节点
+        }
+        return pre;
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(3);
+        Offer24 offer24 = new Offer24();
+        ListNode listNode1 = offer24.createListNode(listNode, 5);
+        ListNode listNode2 = offer24.reverseList(listNode1);
+        System.out.println(listNode2);
+    }
+
+    public ListNode createListNode(ListNode node, int i) {
+        if (i <= 0) {
+            return null;
+        }
+        i--;
+        node.next = createListNode(new ListNode(i), i);
+        return node;
+    }
+}
---
+++ study/src/com/snails/day/chapter02/Office06.java
@@ -0,0 +1,72 @@
+package com.snails.day.chapter02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Snails
+ * @date 2021/11/17 18:22
+ *
+ * 剑指 Offer 06. 从尾到头打印链表
+ *
+ * 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。\
+ *
+ * 示例 1：
+ *
+ * 输入：head = [1,3,2]
+ * 输出：[2,3,1]
+ *
+ *
+ * 限制：
+ *
+ * 0 <= 链表长度 <= 10000
+ */
+public class Office06 {
+
+    private final List<Integer> arr = new ArrayList<>();
+
+    public int[] reversePrint(ListNode head) {
+        each(head);
+        int[] a = new int[arr.size()];
+        for (int i = 0; i < arr.size(); i++) {
+            a[i] = arr.get(i);
+        }
+        return a;
+    }
+
+    public void each(ListNode node) {
+        if (null == node) {
+            return;
+        }
+        each(node.next);
+        arr.add(node.val);
+    }
+
+    public static void main(String[] args) {
+
+        Office06 office06 = new Office06();
+        ListNode listNode = office06.createListNode();
+        int[] ints = office06.reversePrint(listNode);
+        for (int i : ints) {
+            System.out.print(i+"\t");
+        }
+    }
+
+
+    public ListNode createListNode() {
+        ListNode one = new ListNode(1);
+        ListNode two = new ListNode(3);
+        ListNode three = new ListNode(2);
+        one.next = two;
+        two.next = three;
+        return one;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
