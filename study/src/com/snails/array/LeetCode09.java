3a9b53d34f52dd26c446e747ad3657de85103795
snails <202292735@qq.com>
2022-05-26 22:53:42 +0800

    回文数

---
+++ study/src/com/snails/array/LeetCode09.java
@@ -0,0 +1,29 @@
+package com.snails.array;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <a href="https://leetcode.cn/problems/palindrome-number/">回文数</a>
+ *
+ * @since 2022-05-26
+ */
+public class LeetCode09 {
+
+    public static void main(String[] args) {
+        System.out.println(isNumberOfPalindromes(12321));
+    }
+
+    public static boolean isNumberOfPalindromes(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+}
